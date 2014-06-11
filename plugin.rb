7ecf174c6039e532233b07fac611ef5b227e81a1
Shiv Kumar <shiv@newrelic.com>
2014-06-10 23:04:47 -0700

    update zendesk-api client version

--- plugin.rb
+++ plugin.rb
@@ -4,7 +4,7 @@
 
 gem 'faraday_middleware', '0.9.1'
 gem 'inflection', '1.0.0'
-gem 'zendesk_api', '1.3.5'
+gem 'zendesk_api', '1.3.7'
 
 register_asset "javascripts/topic_controller_cont.js"
 register_asset "javascripts/topic_route_cont.js"
@@ -13,13 +13,12 @@ register_asset "stylesheets/buttons_cont.css.scss"
 
 after_initialize do
   load File.expand_path("../controllers/zendesk_controller.rb", __FILE__)
+  load File.expand_path("../lib/zendesk_ticket.rb", __FILE__)
+  load File.expand_path("../lib/new_zendesk_ticket.rb", __FILE__)
+  load File.expand_path("../lib/existing_zendesk_ticket.rb", __FILE__)
 
   Discourse::Application.routes.prepend do
     post 'zendesk/create_ticket' => 'zendesk#create_ticket'
     get 'zendesk/find_ticket' => 'zendesk#find_ticket'
   end
 end
-
-load File.expand_path("../lib/zendesk_ticket.rb", __FILE__)
-load File.expand_path("../lib/new_zendesk_ticket.rb", __FILE__)
-load File.expand_path("../lib/existing_zendesk_ticket.rb", __FILE__)
