d11447fef159529745a6a4c2d3d0b46c504f074c
Shiv Kumar <shiv@newrelic.com>
2014-06-10 23:06:52 -0700

    refactor NewZendeskTicket class

--- lib/new_zendesk_ticket.rb
+++ lib/new_zendesk_ticket.rb
@@ -1,18 +1,20 @@
 class NewZendeskTicket < ZendeskTicket
   def initialize(params)
     super
-    @comment = format_comment(params[:post_url], params[:html_comment])
+    @url = params[:post_url]
+    @html_comment = params[:html_comment]
+    @comment = format_comment
     @ticket = @client.tickets.create(params[:ticket_data].merge(additional_data))
   end
 
   private
 
-  def format_comment(post_url, html_comment)
-    body_to_text = Nokogiri::HTML(html_comment).text
-    return "Link to forum topic:\n#{post_url}\n\nComment:\n#{body_to_text}"
+  def format_comment
+    body_to_text = Nokogiri::HTML(@html_comment).text
+    return "Link to forum topic:\n#{@url}\n\nComment:\n#{body_to_text}"
   end
 
   def additional_data
-    { :comment => { :value => @comment }, :tags => ['forum-beta'] }
+    { :comment => { :value => @comment }, :tags => ['forum-test'] }
   end
 end
